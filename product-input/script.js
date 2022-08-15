6f75dc0e69c2afb01f857d91a0b0a3f28b6e179d
Aline Avelino <aline.savelino@gmail.com>
2022-08-15 18:26:12 -0300

    ✨ feat: update used characters input page

--- product-input/script.js
+++ product-input/script.js
@@ -1,9 +1,27 @@
-let productInput = document.querySelector("input");
+const paragraph = document.querySelector(
+  "#input-container p"
+);
+const productInput = document.querySelector("input");
+
+let maxLength = 60;
+let availableLength = maxLength;
+let usedChar = 0;
+
+paragraph.textContent = `${availableLength}/${maxLength}`;
+
+productInput.addEventListener(
+  "input",
+  updateAvailableLength
+);
+
+function updateAvailableLength(event) {
+  getInputText(event);
+  availableLength = maxLength - usedChar;
+  paragraph.textContent = `${availableLength}/${maxLength}`;
+}
 
 function getInputText(event) {
   let enteredText = event.target.value;
-  console.log(enteredText);
-  console.log(enteredText.length);
+  usedChar = enteredText.length;
+  return usedChar;
 }
-
-productInput.addEventListener("input", getInputText);
