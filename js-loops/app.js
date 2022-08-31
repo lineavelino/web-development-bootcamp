70a5d974060e91d5b02c8b7e4c84af414f8e6506
Aline Avelino <aline.savelino@gmail.com>
2022-08-31 11:48:13 -0300

    ✨ feat: display user data with for in loop

--- js-loops/app.js
+++ js-loops/app.js
@@ -11,6 +11,18 @@ const links = document.querySelectorAll(
   "#highlight-links a"
 );
 
+const userData = {
+  firstname: "Aline",
+  lastname: "Avelino",
+  age: 29,
+};
+const informationButton = document.querySelector(
+  "#user-data button"
+);
+const infosContainer = document.getElementById(
+  "output-user-data"
+);
+
 function sumAllNumbers() {
   let sum = 0;
   for (let i = 0; i <= inputNumber.value; i++) {
@@ -26,5 +38,23 @@ function highlightLinks() {
   }
 }
 
+function displayUserData() {
+  infosContainer.innerHTML = "";
+
+  for (const property in userData) {
+    const liItem = document.createElement("li");
+
+    liItem.textContent = `${property.toUpperCase()}: ${
+      userData[property]
+    }`;
+
+    infosContainer.append(liItem);
+  }
+}
+
 calculatorButton.addEventListener("click", sumAllNumbers);
 highlightButton.addEventListener("click", highlightLinks);
+informationButton.addEventListener(
+  "click",
+  displayUserData
+);
