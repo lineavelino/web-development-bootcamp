5fa71f82403f5c652f42bbeac2af5bf5c7d16e5f
Aline Avelino <aline.savelino@gmail.com>
2022-08-31 14:33:03 -0300

    ✨ feat: roll the dice with while loop

--- js-loops/app.js
+++ js-loops/app.js
@@ -1,16 +1,21 @@
+//#region Calculator
 const inputNumber = document.getElementById("user-number");
 const calculatorButton = document.querySelector(
   "#calculator button"
 );
 const outputSum = document.getElementById("calculated-sum");
+//#endregion
 
+//#region Highlight links
 const highlightButton = document.querySelector(
   "#highlight-links button"
 );
 const links = document.querySelectorAll(
   "#highlight-links a"
 );
+//#endregion
 
+//#region User data
 const userData = {
   firstname: "Aline",
   lastname: "Avelino",
@@ -22,6 +27,24 @@ const informationButton = document.querySelector(
 const infosContainer = document.getElementById(
   "output-user-data"
 );
+//#endregion
+
+//#region Statistics
+const statisticsButton = document.querySelector(
+  "#statistics button"
+);
+const inputNumberDice = document.getElementById(
+  "user-target-number"
+);
+const diceNumbersContainer =
+  document.getElementById("dice-rolls");
+const spanOutputRolls = document.getElementById(
+  "output-total-rolls"
+);
+const spanOutputTarget = document.getElementById(
+  "output-target-number"
+);
+//#endregion
 
 function sumAllNumbers() {
   let sum = 0;
@@ -52,9 +75,37 @@ function displayUserData() {
   }
 }
 
+function rollTheDice() {
+  const targetNumber = inputNumberDice.value;
+  let diceNumber;
+  let roll = 0;
+
+  diceNumbersContainer.innerHTML = "";
+
+  if (targetNumber > 0) {
+    while (targetNumber != diceNumber) {
+      const liItem = document.createElement("li");
+
+      diceNumber = Math.floor(Math.random() * 6) + 1;
+
+      liItem.textContent = `Roll ${
+        roll + 1
+      }: ${diceNumber}`;
+
+      diceNumbersContainer.append(liItem);
+
+      roll++;
+    }
+  }
+
+  spanOutputRolls.textContent = roll;
+  spanOutputTarget.textContent = targetNumber;
+}
+
 calculatorButton.addEventListener("click", sumAllNumbers);
 highlightButton.addEventListener("click", highlightLinks);
 informationButton.addEventListener(
   "click",
   displayUserData
 );
+statisticsButton.addEventListener("click", rollTheDice);
