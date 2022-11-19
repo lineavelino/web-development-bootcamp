2398dd909b7bf6889130881094f70bc6cee83082
Aline Avelino <aline.savelino@gmail.com>
2022-11-19 20:13:50 -0300

    ✨ feat: rendering dynamic content with templates

--- eatwell/app.js
+++ eatwell/app.js
@@ -15,7 +15,19 @@ app.get("/", function (req, res) {
 });
 
 app.get("/restaurants", function (req, res) {
-  res.render("restaurants");
+  const filePath = path.join(
+    __dirname,
+    "data",
+    "restaurants.json"
+  );
+
+  const fileData = fs.readFileSync(filePath);
+
+  const existingRestaurants = JSON.parse(fileData);
+
+  res.render("restaurants", {
+    numberOfRestaurants: existingRestaurants.length,
+  });
 });
 
 app.get("/recommend", function (req, res) {
