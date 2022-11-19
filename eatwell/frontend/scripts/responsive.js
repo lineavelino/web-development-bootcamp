fb8bd75e375c5ac26a8d5df8ee97e5b86e126315
Aline Avelino <aline.savelino@gmail.com>
2022-11-19 01:36:35 -0300

    ✨ feat: creating a starting project

---
+++ eatwell/frontend/scripts/responsive.js
@@ -0,0 +1,10 @@
+const drawerBtnElement =
+  document.getElementById("drawer-btn");
+const mobileDarwerElement =
+  document.getElementById("mobile-drawer");
+
+function toggleDrawer() {
+  mobileDarwerElement.classList.toggle("open");
+}
+
+drawerBtnElement.addEventListener("click", toggleDrawer);
