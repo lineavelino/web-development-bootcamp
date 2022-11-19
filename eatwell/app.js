2647b0fb013db5d4f366235b5c5efac79fa2d66e
Aline Avelino <aline.savelino@gmail.com>
2022-11-19 02:31:26 -0300

    ✨ feat: serving HTML files with Node & Express

--- eatwell/app.js
+++ eatwell/app.js
@@ -1,9 +1,56 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("<h1>Hello, world!</h1>");
+  const indexPath = path.join(
+    __dirname,
+    "views",
+    "index.html"
+  );
+
+  res.sendFile(indexPath);
+});
+
+app.get("/restaurants", function (req, res) {
+  const restaurantsPath = path.join(
+    __dirname,
+    "views",
+    "restaurants.html"
+  );
+
+  res.sendFile(restaurantsPath);
+});
+
+app.get("/recommend", function (req, res) {
+  const recommendPath = path.join(
+    __dirname,
+    "views",
+    "recommend.html"
+  );
+
+  res.sendFile(recommendPath);
+});
+
+app.get("/confirm", function (req, res) {
+  const confirmPath = path.join(
+    __dirname,
+    "views",
+    "confirm.html"
+  );
+
+  res.sendFile(confirmPath);
+});
+
+app.get("/about", function (req, res) {
+  const aboutPath = path.join(
+    __dirname,
+    "views",
+    "about.html"
+  );
+
+  res.sendFile(aboutPath);
 });
 
 app.listen(3000);
