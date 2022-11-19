766c7092ccac1c5f9bc0fe1b86fe2860ae1bfe99
Aline Avelino <aline.savelino@gmail.com>
2022-11-18 23:15:59 -0300

    ✨ feat: add currenttime path - server example

--- node-introduction/app.js
+++ node-introduction/app.js
@@ -1,8 +1,15 @@
 const http = require("http");
 
 function handleRequest(req, res) {
-  res.statusCode = 200;
-  res.end("<h1>Hello World</h1>");
+  if (req.url === '/currenttime') {
+    res.statusCode = 200;
+    res.end("<h1>" + new Date().toISOString() +"</h1>")
+  }
+
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.end("<h1>Hello World</h1>");
+  }
 }
 
 const server = http.createServer(handleRequest);
