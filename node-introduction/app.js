f138a8b7b58b273f028d83c363fd817bd59aaa95
Aline Avelino <aline.savelino@gmail.com>
2022-11-19 00:02:41 -0300

    ✨ feat: parsing user data with Express

--- node-introduction/app.js
+++ node-introduction/app.js
@@ -2,8 +2,17 @@ const express = require("express")
 
 const app = express()
 
+app.use(express.urlencoded({extended: false}))
+
 app.get("/", function(req, res) {
-  res.end("<h1>Hello World</h1>")
+  res.send(
+    `<form action="/store-user" method="POST"><label>Your name:</label><input type="text" name="username"><button>Submit</button></input></form>`
+  );
+})
+
+app.post("/store-user", function(req, res) {
+  const username = req.body.username;
+  res.send(`<h1>Username stored: "${username}"</h1>`);
 })
 
 app.get("/currenttime", function(req, res) {
